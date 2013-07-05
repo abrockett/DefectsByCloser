149ac1ac5506c3f78ec340eda628b644020a6360
= <pairing@rallydev.com>
2013-07-05 17:33:44 -0600

    almost done, just need to possibly incorporate promises

--- App.js
+++ App.js
@@ -1,47 +1,26 @@
-Ext.define('CustomApp', {
-    extend: 'Rally.app.App',
+Ext.define('Rally.apps.defectsbycloser.App', {
+    extend: 'Rally.app.TimeboxScopedApp',
     componentCls: 'app',
+    scopeType: 'release',
 
-    items: [
-        {
-            xtype: 'container',
-            itemId: 'releaseFilter'
-        },
-        {
+    addContent: function() {
+        this.add({
             xtype: 'container',
             itemId: 'grid'
-        }
-    ],
-
-    launch: function() {
-
-        this.down('#releaseFilter').add({
-            xtype: 'rallyreleasecombobox',
-            itemId: 'releaseComboBox',
-            fieldLabel: 'Select Release: ',
-            width: 310,
-            labelWidth: 100,
-            listeners: {
-                ready: this._onReleaseComboboxLoad,
-                change: this._onComboBoxChange,
-                scope: this
-            }
         });
+        this._loadDefects();
     },
 
-    _onComboBoxChange: function(comboBox) {
-        this._defectRecords = [];
-        this._onReleaseComboboxLoad(comboBox);
+    onScopeChange: function() {
+        this._loadDefects();
     },
 
-    _onReleaseComboboxLoad: function(comboBox) {
-        this.releaseComboBox = this.down('#releaseComboBox'); 
-
+    _loadDefects: function() {
         Ext.create('Rally.data.WsapiDataStore', {
             model: 'Defect',
             autoLoad: true,
             fetch: ['FormattedID', 'Name', 'Owner', 'DisplayName', 'UserName', 'RevisionHistory'],
-            filters: [this.releaseComboBox.getQueryFromSelected()],
+            filters: [this.getContext().getTimeboxScope().getQueryFilter()],
             sorters: [{
                 property: 'FormattedID',
                 direction: 'ASC'
@@ -54,79 +33,77 @@ Ext.define('CustomApp', {
     },
 
     _onDefectDataLoaded: function(store, data) {
-        this._customRecords = [];
-        Ext.Array.each(data, function(defect, index) {
-            this._customRecords.push({
+        this._customRecords = Ext.Array.map(data, function(defect) {
+            return {
                 _ref: defect.get('_ref'),
                 FormattedID: defect.get('FormattedID'),
                 Name: defect.get('Name'),
-                ClosedBy: 'not loaded',
-                DateClosed: 'not loaded',
-                RevisionID: Rally.util.Ref.getOidFromRef(defect.get('RevisionHistory')),
-                RevisionNumber: 'not loaded'
-            });
+                ClosedBy: '-',
+                DateClosed: '-',
+                RevisionHistory: defect.get('RevisionHistory'),
+                RevisionNumber: '-'
+            };
         }, this);
 
-        this._getRevisionHistory(data);
+        this._getRevisionHistory();
     },
 
-    _getRevisionHistory: function(defectList) {
-        this._currentDefectList = Ext.clone(defectList);
-        this._defectIndex = 0;
-        var me = this, count;
-        if (defectList.length > 0) {
-            Ext.Array.each(defectList, function(defect) {
-                this._revisionModel = Rally.data.ModelFactory.getModel({
-                    type: 'RevisionHistory',
-                    scope: this,
-                    success: this._onModelCreated
-                });
-                return count;
-            }, this);
+    _getRevisionHistory: function() {
+        this._numberTimesLoaded = 0;
+
+        if (this._customRecords.length > 0) {
+            Rally.data.ModelFactory.getModel({
+                type: 'RevisionHistory',
+                scope: this,
+                success: function(model) {
+                    this._onModelLoaded(model);
+                }
+            });
         } else {
             this._buildGrid();
         }
     },
 
-    _onModelCreated: function(model) {
-        model.load(Rally.util.Ref.getOidFromRef(this._currentDefectList[this._defectIndex].get('RevisionHistory')),{
-            scope: this,
-            success: this._onModelLoaded
-        });
-        this._defectIndex += 1;
+    _onModelLoaded: function(model) {
+        Ext.Array.each(this._customRecords, function(defect, index) {
+            model.load(Rally.util.Ref.getOidFromRef(defect.RevisionHistory), {
+                scope: this,
+                success: function(record) {
+                    this._onRevisionHistoryLoaded(defect, record);
+                }
+            });
+        }, this);
     },
 
-    _onModelLoaded: function(record, operation) {
+    _onRevisionHistoryLoaded: function(defect, record) {
         record.getCollection('Revisions').load({
-            fetch: true,
+            fetch: ['RevisionNumber', 'CreationDate', 'User', 'Description'],
             scope: this,
-            callback: function(revisions, operation, success) {
-                this._onRevisionsLoaded(revisions, record);
+            callback: function(revisions) {
+                this._onRevisionsLoaded(revisions, defect);
             }
         }); 
     },
 
-    _onRevisionsLoaded: function(revisions, record) {
+    _onRevisionsLoaded: function(revisions, defect) {
         Ext.Array.each(revisions, function(revision, revisionIndex) {
-            if (revision.data.Description.search("CLOSED DATE added") !== -1) {
-                Ext.Array.each(this._customRecords, function(customDefect) {
-                    if (customDefect.RevisionID === record.internalId) {
-                        customDefect.RevisionNumber = revision.get('RevisionNumber');
-                        customDefect.DateClosed = revision.get('CreationDate');
-                        customDefect.ClosedBy = revision.get('User')._refObjectName;
-                        return false;
-                    }
-                }, this);
+            if (revision.get('Description').search("CLOSED DATE added") !== -1) {
+                defect.RevisionNumber = revision.get('RevisionNumber');
+                defect.DateClosed = Rally.util.DateTime.formatWithDefault(revision.get('CreationDate'), this.getContext());
+                defect.ClosedBy = revision.get('User')._refObjectName;
                 return false;
             } else if (revisionIndex === (revisions.length-1)) {
-                Ext.Array.each(this._customRecords, function(customDefect) {
-                    if (customDefect && (customDefect.RevisionID === record.internalId)) {
-                        Ext.Array.remove(this._customRecords, customDefect);
-                    }
-                }, this);
+                Ext.Array.remove(this._customRecords, defect);
+                this._numberTimesLoaded--;
             }
         }, this);
-        this._buildGrid();
+
+        this._numberTimesLoaded++;
+
+        if (this._numberTimesLoaded === this._customRecords.length) {
+            this._buildGrid();
+        }
+
     },
 
     _buildGrid: function() {
@@ -139,13 +116,12 @@ Ext.define('CustomApp', {
             this.grid = this.down('#grid').add({
                 xtype: 'rallygrid',
                 store: customStore,
-                showPagingToolbar: false,
                 columnCfgs: [
-                    {text: 'Formatted ID', dataIndex: 'FormattedID', flex: 1, xtype: 'templatecolumn', 
+                    {text: 'Formatted ID', dataIndex: 'FormattedID', xtype: 'templatecolumn', 
                         tpl: Ext.create('Rally.ui.renderer.template.FormattedIDTemplate')},
                     {text: 'Name', dataIndex: 'Name', flex: 2},
                     {text: 'Date Closed', dataIndex: 'DateClosed', flex: 2},
-                    {text: 'Revision Number', dataIndex: 'RevisionNumber', flex: 1},
+                    {text: 'Revision Number', dataIndex: 'RevisionNumber'},
                     {text: 'Closed By', dataIndex: 'ClosedBy', flex: 1}
                 ]
             });
@@ -165,7 +141,7 @@ Ext.define('CustomApp', {
     },
 
     _onButtonPressed: function() {
-        var title = this.down('#releaseComboBox').getRawValue(),
+        var title = this.getContext().getTimeboxScope().getRecord().get('Name'),
             options = "toolbar=1,menubar=1,scrollbars=yes,scrolling=yes,resizable=yes,width=1000,height=500";
 
         // code to get the style that we added in the app.css file
@@ -179,7 +155,7 @@ Ext.define('CustomApp', {
 
         doc.write('<html><head>' + '<style>' + css + '</style><title>' + title + '</title>');
         doc.write('</head><body class="landscape">');
-        doc.write('<p>Release: ' + title + '</p><br />');
+        doc.write('<p style="font-family:Arial,Helvetica,sans-serif;margin:5px">Release: ' + title + '</p><br />');
         doc.write(grid.getEl().dom.innerHTML);
         doc.write('</body></html>');
         doc.close();
